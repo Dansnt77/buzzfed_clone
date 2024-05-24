1ea8817876b3fab9fc76046dabcfa840e18a8e17
Danilo <danilosnt77@gmail.com>
2024-05-24 12:10:55 -0300

    Finish project

---
+++ buzzfeed/src/app/pages/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { QuizzComponent } from "../../components/quizz/quizz.component";
+
+@Component({
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+    imports: [QuizzComponent]
+})
+export class HomeComponent {
+  constructor(){}
+
+  ngOnInit(): void {
+
+  }
+}
